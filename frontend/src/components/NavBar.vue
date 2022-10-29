5705d61fbe4436e6e397f3d70a8fa14c90c204ca
HashEngineering <hashengineeringsolutions@gmail.com>
2022-10-29 09:59:35 -0700

    feat: add cognito support for sign in / sign up

--- frontend/src/components/NavBar.vue
+++ frontend/src/components/NavBar.vue
@@ -1,9 +1,11 @@
 <template>
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-      <a class="navbar-brand" href="#">
-    <img src="../assets/cameraBuddy.png" width="80" height="80" class="d-inline-block" alt="">
-    Pictografy
-  </a>
+      <a class="navbar-brand" href="#"></a>
+      <router-link class="nav-link" to="/">
+        <img src="../assets/cameraBuddy.png" width="80" height="80" class="d-inline-block" alt="">
+        Pictografy
+      </router-link>
+      
       <button
         class="navbar-toggler"
         type="button"
@@ -18,20 +20,38 @@
       <div class="collapse navbar-collapse" id="navbarText">
         <ul class="navbar-nav mx-auto">
           <form class="form-inline">
-    <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-    <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Go</button>
-  </form>
+          <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+          <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Go</button>
+        </form>
         </ul>
-        <button class="btn btn-info mr-3" type="button">Sign Up</button>
-        <button class="btn btn-info" type="button">Sign In</button>
+
+        <button v-if="isLoggedIn()" class="btn btn-info">
+          <router-link class="nav-link" to="profile">Profile</router-link>
+        </button>
+        <button v-if="isLoggedIn()" class="btn btn-info">
+          <router-link class="nav-link" to="upload">Upload</router-link>
+        </button>
+        <button v-if="!isLoggedIn()" class="btn btn-info mr-3" type="button"><router-link class="nav-link" to="register">Sign Up</router-link></button>
+        <button v-if="!isLoggedIn()" class="btn btn-info" type="button"><router-link class="nav-link" to="login">Sign In</router-link></button>
+        
       </div>
     </nav>
   </template>
   
   <script>
   export default {
-      name: 'NavBar'
+      name: 'NavBar',
+      methods: {
+      isLoggedIn: function() {
+        if(this.$cognitoAuth.getCurrentUser() === null) {
+          return false;
+        } else {
+          return true;
+        }
+      }
+    }
   }
+  
   </script>
   
   <style>
