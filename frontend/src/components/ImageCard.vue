e98921e6e9f4d1a9fc673a6da23db6b66f885eaf
Hoovier <javierp2013@csu.fullerton.edu>
2022-10-29 16:08:10 -0700

    Added inf scrolling
    added a few files, get-posts.js will be the place we can put our api call to autopopulate our image boxes

--- frontend/src/components/ImageCard.vue
+++ frontend/src/components/ImageCard.vue
@@ -2,14 +2,15 @@
     <div class="card">
         <img class="card-img-top" :src="img" alt="">
         <div class="card-body">
-            <h5 class="card-title">Caption HERE</h5>
+            <h5 class="card-title">{{post.caption}}</h5>
         </div>
     </div>
 </template>
 
 <script>
-export default {
-  name: "ImageCard",
-  props: ["img"],
-};
+  export default {
+   props: {
+    post: Object
+   }
+  }
 </script>
