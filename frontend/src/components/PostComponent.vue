f992acc3acb444978106f6c01f89e9feaa1fddbb
HashEngineering <hashengineeringsolutions@gmail.com>
2022-11-12 09:59:11 -0800

    fix: add support for tags

--- frontend/src/components/PostComponent.vue
+++ frontend/src/components/PostComponent.vue
@@ -4,15 +4,26 @@
         <div class="card-body">
           <h5 class="card-title">{{post.caption}}</h5>
           <h6 class="card-user">{{post.username}}</h6>
+          <h5 class="card-likes">Likes: {{post.likes}}</h5>
+          <h5 class="card-likes">Dislikes: {{post.dislikes}}</h5>
+          <div v-for="tag in this.post.tags" :post="tag" v-bind:key="tag">
+            <a href="/search?tag={{tag}}">#{{tag}}</a>
+          </div>
         </div>
     </div>
   </template>
   <script>
-    export default {
+  import config from '../config'
+  export default {
      props: {
       post: Object
-     }
+     },
+    methods: {
+      getBackendUrl: function () {
+        return config.prod ? config.backendProd : config.backendLocal;
+      }
     }
+  }
   </script>
 
   
--- frontend/src/get-posts.js
+++ frontend/src/get-posts.js
@@ -16,9 +16,13 @@ const getPosts = (names, start, number) => {
     ret.push({
       imgURL: lastNames[i].imgURL, // this should the actual index
       caption: lastNames[i].title,
-      username: lastNames[i].username
+      username: lastNames[i].username,
+      likes: lastNames[i].likes,
+      dislikes: lastNames[i].dislikes,
+      tags: lastNames[i].tags
     })
   }
+  console.log(ret)
   return ret
 }
 export default getPosts
